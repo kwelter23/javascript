8fd01c3ad3f223bdb57396787384a0d8edf772bc
Welter <kwelter@916schools.org>
2020-09-21 18:35:17 -0500

    before I got help

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -23,16 +23,45 @@ var button = d3.select("#filter-btn");
 
 // Getting a reference to the input element on the page
 var filterDate = d3.select("#datetime");
+    console.log(filterDate);
 
 // Select the Table to Filter
 var filteredTable = d3.select(".tbody");
 
- 
+// This function is triggered when the button is clicked
+function handleClick() {
+    console.log("A button was clicked!");
+
+// We can use d3 to see the object that dispatched the event
+  console.log(d3.event.target);
+}
+
+// We can use the `on` function in d3 to attach an event to the handler function
+button.on("click", handleClick);
+
+// You can also define the click handler inline
+button.on("click", function() {
+  console.log("Hi, a button was clicked!");
+  //enter code to filter table
+  console.log(d3.event.target);
+});
+
+
+  
+
+// Input fields can trigger a change event when new text is entered.
+filterDate.on("click", function() {
+    var newDate = d3.event.target.value;
+    console.log(newDate);
+  });
+   
 
 // Use D3 `.on` to attach a click handler for the date filter
 //button.on("click", function() {
+    //if
+    //    filterDate
     // Filter Data
-  //  var currentCount = parseInt(counter.text());
+  //  var filteredDate = parseInt(counter.text());
   
     // Upvotes should increment the counter
   //  currentCount += 1;
@@ -40,4 +69,14 @@ var filteredTable = d3.select(".tbody");
     // Set the counter h1 text to the new count
    // counter.text(currentCount);
  // });
+  
+ // Create a custom function to return players who made the team
+//function datefilter(sighting) {
+    // return player.madeTeam == true;
+    // A more concise way to express a boolean conditional
+  //  return sighting.datefilter;
+  //}
+  
+  // Call the custom function with filter()
+  //var datefilter = data.filter(sighting);
   
